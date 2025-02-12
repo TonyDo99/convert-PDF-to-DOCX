2ed511114102e3b98695b1057aa85585839984f7
phat.do <dotanphat20@gmail.com>
2025-02-12 13:25:16 +0700

    fix: remove external management packages

--- setup.sh
+++ setup.sh
@@ -35,14 +35,15 @@ pnpm build
 
 # ---------------- Python setup -------------- #
 # Install python
-sudo apt install python3.6
-sudo apt install pipx
-sudo apt install python-dev-is-python3
+apt install python3.12 && apt install pipx && apt install python-dev-is-python3 && apt-get install python3-pip
+
+rm /usr/lib/python3.12/EXTERNALLY-MANAGED
 
 # Go to virtual enviroment
-python3 -m venv venv
-source venv/bin/activate
+# python3 -m venv venv
+# . venv/bin/activate
 
 # Python package management install
-pipx install cookiecutter
-pipx runpip cookiecutter install -r python-packages.txt
+pip install -r python-packages.txt
+# pipx install cookiecutter
+# pipx runpip cookiecutter install -r python-packages.txt
